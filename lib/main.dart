1f1d0b98b224ee1c1e85b636f5f9f2c6355c4517
Jordan-erifried <danerifried@gmail.com>
2022-01-30 19:52:07 +0000

    Adding expanded for all FlatButton

--- lib/main.dart
+++ lib/main.dart
@@ -17,58 +17,73 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playSound(1);
-                }, child: Text(''),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  }, child: Text(''),
+                ),
               ),
 
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playSound(2);
-                }, child: Text(''),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  }, child: Text(''),
+                ),
               ),
 
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  playSound(3);
-                }, child: Text(''),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound(3);
+                  }, child: Text(''),
+                ),
               ),
 
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  playSound(4);
-                }, child: Text(''),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  }, child: Text(''),
+                ),
               ),
 
-              FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  playSound(5);
-                }, child: Text(''),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: () {
+                    playSound(5);
+                  }, child: Text(''),
+                ),
               ),
 
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                }, child: Text(''),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound(6);
+                  }, child: Text(''),
+                ),
               ),
 
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  playSound(7);
-                }, child: Text(''),
-              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound(7);
+                  }, child: Text(''),
+                ),
+              )
             ],
           ),
         ),
